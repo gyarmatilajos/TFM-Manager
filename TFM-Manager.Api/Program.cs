220d5c7c35fe7272cb28e295ad86daec82bc70a2
Gyarmati Lajos <lajos@gyarmati.co>
2026-08-23 20:58:31 +0200

    Basic Api settings done

--- TFM-Manager.Api/Program.cs
+++ TFM-Manager.Api/Program.cs
@@ -1,14 +1,49 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using TFM_Manager.Data.DbContext;
+using TFM_Manager.Data.Entities.Identity;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
 
+if (string.IsNullOrWhiteSpace(connectionString))
+{
+    throw new InvalidOperationException("Connection string 'DefaultConnection' is not configured.");
+}
+
+// Controllers
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+
+// OpenAPI
 builder.Services.AddOpenApi();
+
+// DbContext
+builder.Services.AddDbContext<TfmManagerDbContext>(options =>
+{
+    options.UseSqlServer(connectionString);
+});
+
+// Identity alapregisztráció
+builder.Services
+    .AddIdentityCore<ApplicationUser>(options =>
+    {
+        options.User.RequireUniqueEmail = true;
+
+        options.Password.RequiredLength = 8;
+        options.Password.RequireDigit = true;
+        options.Password.RequireLowercase = true;
+        options.Password.RequireUppercase = true;
+        options.Password.RequireNonAlphanumeric = false;
+    })
+    .AddEntityFrameworkStores<TfmManagerDbContext>()
+    .AddDefaultTokenProviders();
+
+// Jogosultsági szolgáltatások
+builder.Services.AddAuthorization();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
